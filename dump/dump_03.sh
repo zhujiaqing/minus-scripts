63b79126a1290624af7d8dfdf697bdb74c0315ab
xtso520ok <xtso520ok@gmail.com>
2016-05-31 15:54:35 +0800

    add dump_03

--- dump/dump_03.sh
+++ dump/dump_03.sh
@@ -11,7 +11,9 @@ while read uid key
 do
 echo "dump avator $uid - $key"
 url="https://d1uk5e10lg6nan.cloudfront.net/j$key.jpg"
+
 curl -s -o /dev/shm/$key.jpg $url
+
 curl -i -X PUT \
 -H "Content-Type:image/jpeg;charset=UTF-8" \
 --upload-file /dev/shm/$key.jpg \
@@ -19,11 +21,37 @@ curl -i -X PUT \
 
 [ $? -eq 0 ] && mysql -N -h54.169.188.17 -uminus -pminus -Dminus \
 -e"update minus_user_status set avator=1 where uid=$uid"
-break
+
+rm -f /dev/shm/$key.jpg
+
 done
 
 }
 
-init_avator
+init_photo(){
+mysql -N -h54.169.188.17 -uminus -pminus -Dminus \
+-e"select minus_user_photo.uid,minus_user_photo.photo_key from minus_user_photo inner join minus_user_status on minus_user_photo.uid=minus_user_status.uid where photo=0;" | \
+while read uid key
+do
+echo "dump avator $uid - $key"
+url="https://d1uk5e10lg6nan.cloudfront.net/j$key.jpg"
+
+curl -s -o /dev/shm/$key.jpg $url
+
+curl -i -X PUT \
+-H "Content-Type:image/jpeg;charset=UTF-8" \
+--upload-file /dev/shm/$key.jpg \
+"http://resource.api.imyoujia.com/uplusmain-file/resource_type/101?user_id=$uid&albumid=0&optype=0&user_type=3&client_ver=4.0.1-g&token=s00e330000010c43d8ef768417140ca20ce417ba75c41be1c304cdda55efd28791048199c2b99261a0a1149"
+
+[ $? -eq 0 ] && mysql -N -h54.169.188.17 -uminus -pminus -Dminus \
+-e"update minus_user_status set photo=1 where uid=$uid"
+
+rm -f /dev/shm/$key.jpg
+break
+done
+}
+
+#init_avator
+init_photo
 
 
