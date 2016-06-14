95b1b20187a1678da90a9e76789d9aa250c787dc
xtso520ok <xtso520ok@gmail.com>
2016-06-14 11:20:13 +0800

    debug ...

--- dump/start_dump.sh
+++ dump/start_dump.sh
@@ -3,11 +3,11 @@
 dump_20160614(){
 for i in {1..9}
 do
-    echo "screen python meowchat_to_youja.py $(((i-1)*10000)) $((i*10000)) &"
-sleep 3
+    echo "screen python meowchat_to_youja.py $(((i-1)*10000)) $((i*10000)) &" 
+#sleep 3
 done
 
-echo "screen python meowchat_to_youja.py 100000 200000000 &"
+echo "screen python meowchat_to_youja.py 100000 200000000 &" | bash
 }
 
 dump_20160614
