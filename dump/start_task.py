eab3fdf6474cbd4ff4ecb4a4643a1fc3b00af0e3
xtso520ok <xtso520ok@gmail.com>
2016-06-15 11:38:56 +0800

    debug ...

--- dump/start_task.py
+++ dump/start_task.py
@@ -4,10 +4,10 @@ num = 10000
 
 def task(l_min=0, l_max=100):
     print 'screen python ~/minus-scripts/dump/meowchat_to_youja.py 0 %d &\n' % (l_min * num)
-    print 'screen python ~/minus-scripts/dump/meowchat_to_youja.py %d 200000000 &\n' % (l_max * num)
+    print 'screen python ~/minus-scripts/dump/meowchat_to_youja.py %d 200000000 &\n' % ((l_max + 1) * num)
 
     for n in range(l_min, l_max, 1):
-        print 'screen python ~/minus-scripts/dump/meowchat_to_youja.py %d %d &\n' % ((n - 1) * num, n * num)
+        print 'screen python ~/minus-scripts/dump/meowchat_to_youja.py %d %d &\n' % (n * num, (n + 1) * num)
 
 
 if __name__ == '__main__':
