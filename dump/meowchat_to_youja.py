ff61440115fab0a5e0c9786c18e3c64c4d60953b
xtso520ok <xtso520ok@gmail.com>
2016-06-22 16:03:04 +0800

    debug ...

--- dump/meowchat_to_youja.py
+++ dump/meowchat_to_youja.py
@@ -499,14 +499,17 @@ if __name__ == '__main__':
 #     if 3 == len(args):arg = args[1:2]
 #     manual_start(arg)
 
-    start = '01'
-    if 2 == len(args) and ('02' == args[1] or '2' == args[1]):start = '02'
-    if '01' == start:
+    type = '01'
+    if 2 == len(args):
+        if '02' == args[1] or '2' == args[1]:type = '02'
+        elif '03' == args[1] or '3' == args[1]:type = '03'
+    print type
+    if '01' == type:
         mutliprocess_start(start=0, salt=0)
-    elif '02' == start:
+    elif '02' == type:
         mutliprocess_start(start=0, salt=0.75)
-    elif '03' == start:
-        mutliprocess_start(start=0, salt=0.25)
+    elif '03' == type:
+        mutliprocess_start(start=100, salt=0.25)
     
     print '\n[%s] Dump over\n' % time.strftime('%Y-%m-%d %H:%M:%S')
 
