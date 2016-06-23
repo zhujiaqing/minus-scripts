71a556837b109e52e1fce2d2a9f5d6fdddd1fee9
xtso520ok <xtso520ok@gmail.com>
2016-06-24 00:00:52 +0800

    debug ...

--- dump/start_repair.py
+++ dump/start_repair.py
@@ -10,7 +10,7 @@ def manual_start(x):
     dump = Dump()
     
     base_redis = redis.Redis(host="10.154.148.158", port=6379, db=10)
-    uids = [base_redis.spop('S:diff') for i in range(10)]  # @UnusedVariable
+    uids = [base_redis.spop('S:diff') for i in range(1000)]  # @UnusedVariable
     dump.repair(uids)
     dump.close_all()
 
@@ -18,10 +18,8 @@ def repair(process_num=20):
     from multiprocessing import Pool as JPool  # 多进程
     from multiprocessing import cpu_count
     
-    
-#     pool = JPool(process_num * cpu_count())
-    pool = JPool(2)
-    pool.map(manual_start, (i for i in range(3))) 
+    pool = JPool(process_num * cpu_count())
+    pool.map(manual_start, (i for i in range(500))) 
     pool.close()
     pool.join()
 
