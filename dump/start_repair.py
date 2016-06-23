881d3bdda5876ee64c9eb5f21f75862b73f1c3fd
xtso520ok <xtso520ok@gmail.com>
2016-06-23 23:56:29 +0800

    debug ...

--- dump/start_repair.py
+++ dump/start_repair.py
@@ -3,20 +3,25 @@
 
 import redis
 
+from meowchat_to_youja import Dump
+
+
+def manual_start(x):
+    dump = Dump()
+    
+    base_redis = redis.Redis(host="10.154.148.158", port=6379, db=10)
+    uids = [16876896, 16876897, 16876898, 16876899]
+    dump.repair(uids)
+    dump.close_all()
+
 def repair(process_num=20):
     from multiprocessing import Pool as JPool  # 多进程
     from multiprocessing import cpu_count
     
-    from meowchat_to_youja import Dump
-    dump = Dump()
-    
-    base_redis = redis.Redis(host="10.154.148.158", port=6379, db=10)
     
 #     pool = JPool(process_num * cpu_count())
     pool = JPool(2)
-#     pool.map(dump.repair, [base_redis.spop('S:diff') for i in range(10)])  # @UnusedVariable
-
-    pool.map(dump.repair, set([[16876896, 16876897, 16876898], [16876899, 16876900, 16876901], [16876902, 16876903, 16876904, 17171928]])) 
+    pool.map(manual_start, (i for i in range(3))) 
     pool.close()
     pool.join()
 
