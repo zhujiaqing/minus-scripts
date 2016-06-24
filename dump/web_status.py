404f1f50e4887c5ab6bca153d4945318eeb0baad
xtso520ok <xtso520ok@gmail.com>
2016-06-24 23:28:30 +0800

    debug ...

--- dump/web_status.py
+++ dump/web_status.py
@@ -12,29 +12,31 @@ import web
 reload(sys)
 sys.setdefaultencoding('UTF-8')  # @UndefinedVariable
 
-usa_redis = redis.Redis(host="10.154.148.158", port=6379, db=5)
-sg_mysql = MySQLdb.connect(host='54.169.234.201', user='minus', passwd='minus', charset='utf8', db='uplusmain', port=3306)
+usa_redis_5 = redis.Redis(host="10.154.148.158", port=6379, db=5)
+usa_redis_10 = redis.Redis(host="10.154.148.158", port=6379, db=10)
 
 def get_meow():
-    b_timestamp = usa_redis.hget('H:scale', 'meow_timestamp')
-    b_num = usa_redis.hget('H:scale', 'meow_num')
+    b_timestamp = usa_redis_5.hget('H:scale', 'meow_timestamp')
+    b_num = usa_redis_5.hget('H:scale', 'meow_num')
     b_timestamp = 0 if not b_timestamp else int(b_timestamp)
     b_num = 0 if not b_num else int(b_num)
     
-    info = usa_redis.info('Keyspace')
+    info = usa_redis_5.info('Keyspace')
     num = info['db1']['keys']
     
     timestamp = int(time.time())
-    usa_redis.hset('H:scale', 'meow_num', num)
-    usa_redis.hset('H:scale', 'meow_timestamp', timestamp)
+    usa_redis_5.hset('H:scale', 'meow_num', num)
+    usa_redis_5.hset('H:scale', 'meow_timestamp', timestamp)
     
     if timestamp == b_timestamp:timestamp = 1
         
     return num, (num - b_num) / (timestamp - b_timestamp)
 
 def get_youja():
-    b_timestamp = usa_redis.hget('H:scale', 'youja_timestamp')
-    b_num = usa_redis.hget('H:scale', 'youja_num')
+    sg_mysql = MySQLdb.connect(host='54.169.234.201', user='minus', passwd='minus', charset='utf8', db='uplusmain', port=3306)
+
+    b_timestamp = usa_redis_5.hget('H:scale', 'youja_timestamp')
+    b_num = usa_redis_5.hget('H:scale', 'youja_num')
     b_timestamp = 0 if not b_timestamp else int(b_timestamp)
     b_num = 0 if not b_num else int(b_num)
     
@@ -44,8 +46,8 @@ def get_youja():
     cur.close()
     
     timestamp = int(time.time())
-    usa_redis.hset('H:scale', 'youja_num', num)
-    usa_redis.hset('H:scale', 'youja_timestamp', timestamp)
+    usa_redis_5.hset('H:scale', 'youja_num', num)
+    usa_redis_5.hset('H:scale', 'youja_timestamp', timestamp)
     
     if timestamp == b_timestamp:timestamp = 1
     
@@ -96,10 +98,13 @@ class meow:
         web.header('Cache-Control', 'no-Cache') 
         web.header('Refresh', 15)
         meow_num, meow_speed = get_meow()
-        return '<H1>[%s] meow num is %s, speed is %s /s</H1>' % (
+        return '<H1>[%s] meow num is %s, speed is %s /s' \
+            '<BR/>cur task num is %s' \
+            '</H1>' % (
                                               time.strftime('%Y-%m-%d %H:%M:%S'),
                                               format(meow_num, ','),
-                                              format(meow_speed, ',')
+                                              format(meow_speed, ','),
+                                              format(usa_redis_10.scard('S:diff') if usa_redis_10.exists('S:diff') else 0, ',')
                                               )
 
 if __name__ == "__main__":
