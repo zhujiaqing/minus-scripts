59b22e53c2befc40a04e73e729a65b51769b5bbd
xtso520ok <xtso520ok@gmail.com>
2016-07-15 17:31:22 +0800

    debug bin/* usa/*

--- usa/dump_increment.py
+++ usa/dump_increment.py
@@ -9,18 +9,19 @@ import redis
 from meowchat_user import DumpUser
 from meowchat_photo import DumpPhoto
 
-KEY_SADD_DIFF_USER = 'S:diff:user'
-KEY_SADD_DIFF_PHOTO = 'S:diff:photo'
+timestamp = time.strftime('%Y%m%d%H%M%S')
+KEY_SADD_DIFF_USER = 'S:diff:user:%s' % timestamp
+KEY_SADD_DIFF_PHOTO = 'S:diff:photo:%s' % timestamp
 
 def loading_increment_user():
     usa_redis_10 = redis.Redis(host="10.154.148.158", port=6666, db=10)    
     
-    dumpUser = DumpUser()
-    dumpPhoto = DumpPhoto()
-
-    usa_mysql = MySQLdb.connect(host='10.231.129.198', user='root', passwd='carlhu', charset='utf8', db='minus', port=3306)
-    cur = usa_mysql.cursor()
     while True:
+        dumpUser = DumpUser()
+        dumpPhoto = DumpPhoto()
+
+        usa_mysql = MySQLdb.connect(host='10.231.129.198', user='root', passwd='carlhu', charset='utf8', db='minus', port=3306)
+        cur = usa_mysql.cursor()
         user_sql = 'select id from minus_user where id>%s limit 100' % usa_redis_10.get('meow:max:uid')
         size = cur.execute(user_sql)
         
@@ -35,14 +36,14 @@ def loading_increment_user():
             usa_redis_10.sadd(KEY_SADD_DIFF_USER, user[0])
             usa_redis_10.sadd(KEY_SADD_DIFF_PHOTO, user[0])
         
+        cur.close()
+        usa_mysql.close()
+        
         dumpUser.repair_increment(KEY_SADD_DIFF_USER)
         dumpPhoto.repair_increment(KEY_SADD_DIFF_PHOTO)
 
-    cur.close()
-    usa_mysql.close()
-
-    dumpUser.close_all()
-    dumpPhoto.close_all()
+        dumpUser.close_all()
+        dumpPhoto.close_all()
     
 if __name__ == '__main__':
     loading_increment_user()
--- usa/repair_special.py
+++ usa/repair_special.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
 # -*-coding:UTF8-*-
 
+'''
+
+    # atschx 16953316
+    # 8ops2016 17172928
+#     usa_redis_10.sadd(KEY_SADD_DIFF_USER, 16953316)
+#     usa_redis_10.sadd(KEY_SADD_DIFF_PHOTO, 16953316)
+#     return 
+    
+'''
 import time
 
 import MySQLdb
@@ -16,22 +25,16 @@ KEY_SADD_DIFF_PHOTO = 'S:diff:photo:%s' % timestamp
 def get_uids_by_photouri_is_null():
     usa_redis_10 = redis.Redis(host="10.154.148.158", port=6666, db=10)
     
-    dumpUser = DumpUser()
-    dumpPhoto = DumpPhoto()
-    
-    
-    # atschx 16953316
-    # 8ops2016 17172928
-#     usa_redis_10.sadd(KEY_SADD_DIFF_USER, 16953316)
-#     usa_redis_10.sadd(KEY_SADD_DIFF_PHOTO, 16953316)
-#     return 
-    
-    sg_mysql_10 = MySQLdb.connect(host='54.169.234.201', user='uplus', passwd='q1w2e3r4t5', charset='utf8', db='uplus_resource', port=3306)
-    cur = sg_mysql_10.cursor()
     
     max_id = 0
     
     while True:
+        dumpUser = DumpUser()
+        dumpPhoto = DumpPhoto()
+        
+        sg_mysql_10 = MySQLdb.connect(host='54.169.234.201', user='uplus', passwd='q1w2e3r4t5', charset='utf8', db='uplus_resource', port=3306)
+        cur = sg_mysql_10.cursor()
+
         user_sql = 'select id,user_id from photos where photouri = "" and id>%s limit 100' % max_id
         size = cur.execute(user_sql)
         
@@ -46,14 +49,14 @@ def get_uids_by_photouri_is_null():
             usa_redis_10.sadd(KEY_SADD_DIFF_USER, photo[1])
             usa_redis_10.sadd(KEY_SADD_DIFF_PHOTO, photo[1])
         
+        cur.close()
+        sg_mysql_10.close()
+
         dumpUser.repair_photo(KEY_SADD_DIFF_USER)
         dumpPhoto.repair_increment(KEY_SADD_DIFF_PHOTO)
         
-    cur.close()
-    sg_mysql_10.close()
-    
-    dumpUser.close_all()
-    dumpPhoto.close_all()
+        dumpUser.close_all()
+        dumpPhoto.close_all()
 
 if __name__ == '__main__':
     get_uids_by_photouri_is_null()
