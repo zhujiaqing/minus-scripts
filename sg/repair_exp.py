8d97c9ffebf6e61d3e1365b81d33c0f68357a079
xtso520ok <xtso520ok@gmail.com>
2016-07-20 11:39:26 +0800

    mod ...

--- sg/repair_exp.py
+++ sg/repair_exp.py
@@ -4,52 +4,59 @@
 import redis
 
 
-rinfo = redis.Redis(host='jedisbuilderinfo.redis.youja.cn', port=6379, db=6)
-rexp = redis.Redis(host='userexp.redis.youja.cn', port=6801, db=6)
+def repair_exp(keyword='111115*'):
+    rinfo = redis.Redis(host='jedisbuilderinfo.redis.youja.cn', port=6379, db=6)
+    rexp = redis.Redis(host='userexp.redis.youja.cn', port=6801, db=6)
 
-glevearr = [0,
-         250, 500,
-         1250, 2000,
-         3000, 4000,
-         5000, 10000,
-         35000, 65000,
-         100000, 200000,
-         350000, 4750000,
-         5000000, 10000000,
-         15000000, 20000000,
-         25000000, 30000000,
-         35000000, 40000000,
-         45000000, 50000000,
-         55000000, 60000000,
-         65000000, 70000000,
-         75000000, 80000000
-        ]
+    glevearr = [0,
+             250, 500,
+             1250, 2000,
+             3000, 4000,
+             5000, 10000,
+             35000, 65000,
+             100000, 200000,
+             350000, 4750000,
+             5000000, 10000000,
+             15000000, 20000000,
+             25000000, 30000000,
+             35000000, 40000000,
+             45000000, 50000000,
+             55000000, 60000000,
+             65000000, 70000000,
+             75000000, 80000000
+            ]
+    
+    keys = rinfo.keys('hExp:%' % keyword)
+    
+    for key in keys:
+        try:
+            g = int(rinfo.hget(key, 'G'))
+            gl = gnv = gnp = 0
+            if g > glevearr[-1]:
+                gl = len(glevearr) - 1
+            else:
+                for level in range(len(glevearr)):
+                    if g < glevearr[level + 1]:
+                        gl = level
+                        break
+                gnv = glevearr[gl + 1] - g
+                gnp = int((g - glevearr[gl]) * 100.00 / (glevearr[gl + 1] - glevearr[gl]))
+            
+            mset_val = {'GL':gl, 'GNV':gnv, 'GNP':gnp}
+            
+            rexp.hmget(key, rinfo.hgetall(key))
+            rexp.hmget(key, mset_val)
+        except Exception as ex:
+            print ex
 
-keys = rinfo.keys('hExp:%s' % '111115*')
-print keys
+def del_old_exp(keyword='11111526*'):
+    rinfo = redis.Redis(host='jedisbuilderinfo.redis.youja.cn', port=6379, db=6)
+    keys = rinfo.keys('hExp:%' % keyword)
+    for key in keys:
+        rinfo.delete(key)
+    
+if __name__ == '__main__':
+    repair_exp('*')
+    
+    print '\nCompleted\n'
 
-for key in keys:
-    g = int(rinfo.hget(key, 'G'))
-    gl = gnv = gnp = 0
-    if g > glevearr[-1]:
-        gl = len(glevearr) - 1
-    else:
-        for level in range(len(glevearr)):
-            if g < glevearr[level + 1]:
-                gl = level
-                break
-        gnv = glevearr[gl + 1] - g
-        gnp = int((g - glevearr[gl]) * 100.00 / (glevearr[gl + 1] - glevearr[gl]))
-    
-    mset_val = {'GL':gl, 'GNV':gnv, 'GNP':gnp}
-    
-    old_info = rinfo.hgetall(key)
-    print type(old_info), old_info
-    rexp.hmget(key, rinfo.hgetall(key))
-    rexp.hmget(key, mset_val)
-#     rinfo.delete(key)
-    print key, mset_val
-    break
-    
-    
-    
