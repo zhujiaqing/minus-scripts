b22cc31645acdd3ab709c983e990168f628ece76
xtso520ok <xtso520ok@gmail.com>
2016-07-20 10:37:16 +0800

    add exp

---
+++ sg/repair_exp.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding:utf8 -*-
+
+import redis
+
+
+rinfo = redis.Redis(host='jedisbuiderinfo.redis.youja.cn', port=6379, db=6)
+
+
+glevearr = [0,
+         250, 500,
+         1250, 2000,
+         3000, 4000,
+         5000, 10000,
+         35000, 65000,
+         100000, 200000,
+         350000, 4750000,
+         5000000, 10000000,
+         15000000, 20000000,
+         25000000, 30000000,
+         35000000, 40000000,
+         45000000, 50000000,
+         55000000, 60000000,
+         65000000, 70000000,
+         75000000, 80000000
+        ]
+
+keys = rinfo.keys('hExp:%s' % '1111111*')
+print keys
+
+for key in keys:
+    g = int(rinfo.hget(key, 'G'))
+    gl = gnv = gnp = 0
+    if g > glevearr[-1]:
+        gl = len(glevearr) - 1
+    else:
+        for level in range(len(glevearr)):
+            if g < glevearr[level + 1]:
+                gl = level
+                break
+        gnv = glevearr[gl + 1] - g
+        gnp = int((g - glevearr[gl]) * 100.00 / (glevearr[gl + 1] - glevearr[gl]))
+    
+    mset_val = {'GL':gl, 'GNV':gnv, 'GNP':gnp}
+    rinfo.mset(key, mset_val)
+    print key, mset_val
+    
+    
+    
