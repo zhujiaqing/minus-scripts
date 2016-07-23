90c9e117abb88af0c4cd078789fda280101bf194
xtso520ok <xtso520ok@gmail.com>
2016-07-23 21:19:18 +0800

    debug ...

---
+++ sg/check_user_relation.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding:utf8 -*-
+
+import os
+
+import redis
+
+
+def check_by_file(uid_file):
+    if uid_file is None:return
+    if not os.path.exists(uid_file):return
+    
+    print 'OK'
+    
+#     r_rf = redis.Redis(host='user_relation.redis.youja.cn', port=6802, db=5)
+#     r_rt = redis.Redis(host='user_relation.redis.youja.cn', port=6802, db=8)
+
+        
+if __name__ == '__main__':
+    check_by_file(uid_file='~/backup/uid-20160723')
+    
+    print '\nCompleted\n'
+
