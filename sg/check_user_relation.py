e9c4cf917a429091c7455137900fa924200f30f6
xtso520ok <xtso520ok@gmail.com>
2016-07-23 21:36:33 +0800

    debug ...

--- sg/check_user_relation.py
+++ sg/check_user_relation.py
@@ -9,15 +9,16 @@ import redis
 def check_by_file(uid_file):
     if uid_file is None or not os.path.exists(uid_file):return
     
+    r_rf = redis.Redis(host='user_relation.redis.youja.cn', port=6802, db=5)
+    r_rt = redis.Redis(host='user_relation.redis.youja.cn', port=6802, db=8)
+
     with open(uid_file, 'r') as uf:
         for uid in uf.readlines():
-            if '-' == uid:continue
-            print uid
+            print '%12s - rf - %04d' % (uid, r_rf.zcard('U:rf:%s' % uid))
+            print '%12s - rf - %04d' % (uid, r_rt.zcard('U:rt:%s' % uid))
             break
-    
-#     r_rf = redis.Redis(host='user_relation.redis.youja.cn', port=6802, db=5)
-#     r_rt = redis.Redis(host='user_relation.redis.youja.cn', port=6802, db=8)
-
+        
+        uf.close()
         
 if __name__ == '__main__':
     check_by_file(uid_file='/home/jesse/backup/uid-20160723')
