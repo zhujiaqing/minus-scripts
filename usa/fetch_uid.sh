2f6af6cfb5b07bbe1f772d60fae37dc3efa9a1ad
xtso520ok <xtso520ok@gmail.com>
2016-07-25 17:31:15 +0800

    add fetch uid bash

--- usa/fetch_uid.sh
+++ usa/fetch_uid.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 fetch(){
-    sfile="s3://youja-logs/nginx/$(date +%Y%m%d)/members.api.imyoujia.com_access.log-$(date +%Y%m%d%H).gz"
+    sfile="s3://youja-logs/nginx/$(date +%Y%m%d)/moplus.api.imyoujia.com_access.log-$(date -d '-1 hours' +%Y%m%d%H).gz"
     tfile="/dev/shm/moplus-$(date +%s).gz"
     
     /usr/local/python/bin/aws s3 cp $sfile $tfile
-    zcat $tfile | awk -F'`' '$17<100000000{print $17}' | sort -u > $tfile-uid
+    /bin/zcat $tfile | awk -F'`' '$17<100000000{print $17}' | sort -u > $tfile-uid
     /usr/local/redis/bin/redis-cli -p 6666 -n 26 sadd S:Task:relation $(paste -s -d' ')
-    rm -f $tfile $tfile-uid
+    /bin/rm -f $tfile $tfile-uid
 }
 
 fetch
