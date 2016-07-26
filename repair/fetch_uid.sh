e290a789922522e4f1f6eb95bbc62198b2e37415
jqzhu <jqzhu2000@gmail.com>
2016-07-26 10:45:47 +0800

    add

--- repair/fetch_uid.sh
+++ repair/fetch_uid.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
 fetch(){
-    sfile="s3://youja-logs/nginx/$(date +%Y%m%d)/moplus.api.imyoujia.com_access.log-$(date -d '-1 hours' +%Y%m%d%H).gz"
+    tag=$1
+    [ -z $tag ] && return
+    sfile="s3://youja-logs/nginx/$tag/$(date +%Y%m%d)/moplus.api.imyoujia.com_access.log-$(date -d '-1 hours' +%Y%m%d%H).gz"
     tfile="/dev/shm/moplus-$(date +%s).gz"
-    
+
+    /usr/local/python/bin/aws s3 ls $sfile || return
     /usr/local/python/bin/aws s3 cp $sfile $tfile
     /bin/zcat $tfile | awk -F'`' '$17<100000000{print $17}' | sort -u > $tfile-uid
     /usr/local/redis/bin/redis-cli -p 6666 -n 26 sadd S:Task:relation $(paste -s -d' ' $tfile-uid )
     /bin/rm -f $tfile $tfile-uid
 }
 
-fetch
+fetch nginx_10_13.youja.cn
+fetch nginx_10_14.youja.cn
 
 exit 0
