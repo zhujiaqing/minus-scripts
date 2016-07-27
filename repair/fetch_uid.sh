fa0dddb87f96b494f13c646f64781837e137e1b1
jqzhu <jqzhu2000@gmail.com>
2016-07-27 18:39:17 +0800

    add

--- repair/fetch_uid.sh
+++ repair/fetch_uid.sh
@@ -10,6 +10,7 @@ fetch(){
     /usr/local/python/bin/aws s3 cp $sfile $tfile
     /bin/zcat $tfile | awk -F'`' '$17<100000000{print $17}' | sort -u > $tfile-uid
     /usr/local/redis/bin/redis-cli -p 6666 -n 26 sadd S:Task:relation $(paste -s -d' ' $tfile-uid )
+    /usr/local/redis/bin/redis-cli -p 6666 -n 26 srem S:Task:relation -
     /bin/rm -f $tfile $tfile-uid
 }
 
